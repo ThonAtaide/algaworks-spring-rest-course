74444768021ed08993a4ea7f443d11dafbcd0278
ThonAtaide <thon_ataide10@hotmail.com>
2023-09-18 15:09:54 -0300

    project created

---
+++ src/main/java/com/algaworks/algaworksspringrestcourse/AlgaworksSpringRestCourseApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algaworksspringrestcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgaworksSpringRestCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgaworksSpringRestCourseApplication.class, args);
+    }
+
+}
