d7fe34d3c2ed7cc104ba60411d025c9d1a8373aa
ThonAtaide <thon_ataide10@hotmail.com>
2023-10-07 19:24:04 -0300

    learning about jpa keywords, query annotation and xml query file

--- src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
@@ -4,7 +4,15 @@ import com.algaworks.algafood.domain.model.Cozinha;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface CozinhaRepository extends JpaRepository<Cozinha, Long> {
 
+    List<Cozinha> findTodasByNomeContaining(String nome);
+
+    Optional<Cozinha> findFirstCozinhaByNome(String nome);
+
+    List<Cozinha> findTop2ByNomeContaining(String nome);
 }
--- src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -2,9 +2,15 @@ package com.algaworks.algafood.domain.repository;
 
 import com.algaworks.algafood.domain.model.Restaurante;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
 
+    List<Restaurante> findRestauranteByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
+
+//    @Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
+    List<Restaurante> buscaPorNomeECozinha(String nome, @Param("id") Long cozinha);
 }
